e50f21795095e15db596499bdbc0c9e8528c5cb1
Pupinder Singh <pathaniarajput92@gmail.com>
2023-08-23 10:34:44 +0530

    Added getAllDevices functionality

--- public/js/index.js
+++ public/js/index.js
@@ -12,3 +12,26 @@ let camAllowed = 1;
 let videoDevices = [];
 let mediaConstraints = { video: true, audio: true };
 
+async function getAllDevices() {
+  if (!navigator.mediaDevices?.enumerateDevices) {
+    console.log("enumerateDevices() is not supported by the browser.");
+  } else {
+    try {
+      let devices = await navigator.mediaDevices.enumerateDevices();
+      devices.forEach((device, index) => {
+        if (device.kind === "videoinput") {
+          console.log(index, device);
+          videoDevices.push(device);
+          const option = document.createElement("option");
+          option.value = device.deviceId;
+          option.text = device.label || `Camera ${cameraDropdown.length + 1}`;
+          cameraDropdown.appendChild(option);
+        }
+      });
+    } catch (err) {
+      console.error(`${err.name}: ${err.message}`);
+    }
+  }
+}
+
+getAllDevices();
