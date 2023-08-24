9821536305b64c17031ca8f0f6a90ea10a81b3da
Pupinder Singh <pathaniarajput92@gmail.com>
2023-08-24 12:59:58 +0530

    Added unique identifier uuid4 functionality

--- public/js/index.js
+++ public/js/index.js
@@ -47,9 +47,15 @@ async function updateCameraLocalStream() {
 
 function switchCamera(deviceId) {
   mediaConstraints.video = { deviceId: { exact: deviceId } };
-  console.log({ mediaConstraints });
-
   updateCameraLocalStream();
+}
+
+function uuidv4() {
+  return "xxyxyxxyx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
 
 getAllDevices();
