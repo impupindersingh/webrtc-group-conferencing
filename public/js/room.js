6535708e9d71c63052ecef7db3c62a57381b2b31
Pupinder Singh <pathaniarajput92@gmail.com>
2023-08-27 18:02:51 +0530

    Added room page client js

---
+++ public/js/room.js
@@ -0,0 +1,26 @@
+const socket = io();
+const myvideo1 = document.querySelector("#vd1");
+const myvideo2 = document.querySelector("#vd2");
+const myvideo3 = document.querySelector("#vd3");
+let videoFream = 1;
+const roomid = params.get("room");
+let username;
+const chatRoom = document.querySelector(".chat-cont");
+const sendButton = document.querySelector(".chat-send");
+const messageField = document.querySelector(".chat-input");
+const videoContainer = document.querySelector("#vcont");
+const overlayContainer = document.querySelector("#overlay");
+const continueButt = document.querySelector(".continue-name");
+const nameField = document.querySelector("#name-field");
+const videoButt = document.querySelector(".novideo");
+const audioButt = document.querySelector(".audio");
+const cutCall = document.querySelector(".cutcall");
+const screenShareButt = document.querySelector(".screenshare");
+const whiteboardButt = document.querySelector(".board-icon");
+const cameraDropdown = document.getElementById("camera-select");
+cameraDropdown.innerHTML = "";
+//whiteboard js start
+const whiteboardCont = document.querySelector(".whiteboard-cont");
+const canvas = document.querySelector("#whiteboard");
+const ctx = canvas.getContext("2d");
+
