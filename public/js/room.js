51e1b22bae31f8c43cc67f9ce5c51efaf18b418d
Pupinder Singh <pathaniarajput92@gmail.com>
2023-09-08 17:26:57 +0530

    Added handleCallActions socket.io event

--- public/js/room.js
+++ public/js/room.js
@@ -502,6 +502,25 @@ cutCall.addEventListener("click", () => {
   location.href = "/";
 });
 
+function handleCallActions(msg, sid) {
+  if (msg == "mute") {
+    console.log(sid + " muted themself");
+    document.querySelector(`#mute${sid}`).style.visibility = "visible";
+    micInfo[sid] = "off";
+  } else if (msg == "unmute") {
+    console.log(sid + " unmuted themself");
+    document.querySelector(`#mute${sid}`).style.visibility = "hidden";
+    micInfo[sid] = "on";
+  } else if (msg == "videooff") {
+    console.log(sid + "turned video off");
+    document.querySelector(`#vidoff${sid}`).style.visibility = "visible";
+    videoInfo[sid] = "off";
+  } else if (msg == "videoon") {
+    console.log(sid + "turned video on");
+    document.querySelector(`#vidoff${sid}`).style.visibility = "hidden";
+    videoInfo[sid] = "on";
+  }
+}
 // Added socket events
 
 socket.on("action", handleCallActions);
