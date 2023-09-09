a468a9bf320447781ec98b7f99918a43aaedc7f3
Pupinder Singh <pathaniarajput92@gmail.com>
2023-09-09 22:29:24 +0530

    Added handleVideoOffer socket.io event

--- public/js/room.js
+++ public/js/room.js
@@ -530,6 +530,110 @@ function handleVideoContainer(count) {
   }
 }
 
+function handleVideoOffer(offer, sid, cname, micinf, vidinf) {
+  cName[sid] = cname;
+  console.log("video offered recevied");
+  micInfo[sid] = micinf;
+  videoInfo[sid] = vidinf;
+  connections[sid] = new RTCPeerConnection(configuration);
+
+  connections[sid].onicecandidate = function (event) {
+    if (event.candidate) {
+      console.log("icecandidate fired");
+      socket.emit("new icecandidate", event.candidate, sid);
+    }
+  };
+
+  connections[sid].ontrack = function (event) {
+    if (!document.getElementById(sid)) {
+      console.log("track event fired");
+      let vidCont = document.createElement("div");
+      let newvideo = document.createElement("video");
+      let name = document.createElement("div");
+      let muteIcon = document.createElement("div");
+      let videoOff = document.createElement("div");
+      videoOff.classList.add("video-off");
+      muteIcon.classList.add("mute-icon");
+      name.classList.add("nametag");
+      name.innerHTML = `${cName[sid]}`;
+      vidCont.id = sid;
+      muteIcon.id = `mute${sid}`;
+      videoOff.id = `vidoff${sid}`;
+      muteIcon.innerHTML = `<i class="fas fa-microphone-slash"></i>`;
+      videoOff.innerHTML = "Video Off";
+      vidCont.classList.add("video-box");
+      newvideo.classList.add("video-frame");
+      newvideo.autoplay = true;
+      newvideo.playsinline = true;
+      newvideo.id = `video${sid}`;
+      newvideo.srcObject = event.streams[0];
+
+      if (micInfo[sid] == "on") muteIcon.style.visibility = "hidden";
+      else muteIcon.style.visibility = "visible";
+
+      if (videoInfo[sid] == "on") videoOff.style.visibility = "hidden";
+      else videoOff.style.visibility = "visible";
+
+      vidCont.appendChild(newvideo);
+      vidCont.appendChild(name);
+      vidCont.appendChild(muteIcon);
+      vidCont.appendChild(videoOff);
+
+      videoContainer.appendChild(vidCont);
+    }
+  };
+
+  connections[sid].onremovetrack = function (event) {
+    if (document.getElementById(sid)) {
+      document.getElementById(sid).remove();
+      console.log("removed a track");
+    }
+  };
+
+  connections[sid].onnegotiationneeded = function () {
+    connections[sid]
+      .createOffer()
+      .then(function (offer) {
+        return connections[sid].setLocalDescription(offer);
+      })
+      .then(function () {
+        socket.emit("video-offer", connections[sid].localDescription, sid);
+      })
+      .catch(reportError);
+  };
+
+  let desc = new RTCSessionDescription(offer);
+
+  connections[sid]
+    .setRemoteDescription(desc)
+    .then(() => {
+      return navigator.mediaDevices.getUserMedia(mediaConstraints);
+    })
+    .then((localStream) => {
+      localStream.getTracks().forEach((track) => {
+        connections[sid].addTrack(track, localStream);
+        console.log("added local stream to peer");
+        if (track.kind === "audio") {
+          audioTrackSent[sid] = track;
+          if (!audioAllowed) audioTrackSent[sid].enabled = false;
+        } else {
+          videoTrackSent[sid] = track;
+          if (!videoAllowed) videoTrackSent[sid].enabled = false;
+        }
+      });
+    })
+    .then(() => {
+      return connections[sid].createAnswer();
+    })
+    .then((answer) => {
+      return connections[sid].setLocalDescription(answer);
+    })
+    .then(() => {
+      socket.emit("video-answer", connections[sid].localDescription, sid);
+    })
+    .catch(handleGetUserMediaError);
+}
+
 // Added socket events
 
 socket.on("action", handleCallActions);
